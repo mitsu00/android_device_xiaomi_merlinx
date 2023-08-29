a1a0ae88d9401d9f1c41d141e6f65ee4c81df034
mitsu00 <dev@ozip.my.id>
2023-08-29 12:13:19 +0700

    merlinx: Evolution X Bringup

--- device.mk
+++ device.mk
@@ -42,10 +42,11 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     FrameworksResOverlayMerlinx \
+    SettingsOverlayMerlinx \
     SystemUIOverlayMerlinx
 
 # Rootdir
